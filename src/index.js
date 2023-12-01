e2901747103c31505ef6ae17520fe8b8f6b04eed
Marco Starker <marco.starker@gmx.net>
2023-12-01 19:16:20 +0100

    feat: init

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import https from 'https';
+
+export const handler = (event) => {
+    return new Promise((resolve, reject) => {
+        console.log('Request', JSON.stringify(event));
+
+        const request = https.request(
+            {
+                hostname:           'homeassistant.dasred.de',
+                path:               '/auth/token',
+                port:               process.env.AHC_HOMEASSISTANT_PORT ?? 443,
+                method:             event.httpMethod,
+                rejectUnauthorized: false,
+                family:             6,
+                headers:            (({Host, ['User-Agent']: nuff, ['accept-encoding']: narf, ...headers}) => headers)(event.headers),
+            },
+            (response) => {
+                let body = '';
+                response.setEncoding('utf8');
+                response.on('data', (chunk) => body += chunk);
+                response.on('end', () => {
+                    const resultBody = {
+                        isBase64Encoded: false,
+                        statusCode:      response.statusCode || 500,
+                        headers:         response.headers,
+                        body,
+                    };
+
+                    console.log('Response', JSON.stringify(resultBody));
+                    if (response.status >= 400) {
+                        return reject(resultBody);
+                    }
+
+                    resolve(resultBody);
+                });
+            }
+        );
+        request.write(event.isBase64Encoded ? atob(event.body) : event.body);
+        request.end();
+    });
+};
